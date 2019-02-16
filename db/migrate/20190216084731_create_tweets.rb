9b97944dc589643731acbfe72d25ab38f4855ca2
colorbox <colorbox222@gmail.com>
2019-02-16 17:52:44 +0900

    Add user and tweet

---
+++ db/migrate/20190216084731_create_tweets.rb
@@ -0,0 +1,10 @@
+class CreateTweets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tweets do |t|
+      t.references :user, null: false
+      t.string :text, null:false, default: ''
+
+      t.timestamps
+    end
+  end
+end

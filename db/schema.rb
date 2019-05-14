ce7f17353a0a8d828b4597939cbda6a57c419b69
colorbox <colorbox222@gmail.com>
2019-05-14 21:35:00 +0900

    Update schema
     - update default value for User.name
     - migrate kaomoji

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_16_084731) do
+ActiveRecord::Schema.define(version: 2019_05_13_140711) do
+
+  create_table "kaomojis", force: :cascade do |t|
+    t.integer "tweet_id", null: false
+    t.string "kaomoji", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tweet_id"], name: "index_kaomojis_on_tweet_id"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.integer "user_id", null: false
-    t.string "tweet_identifier", default: "f", null: false
+    t.string "tweet_identifier", default: "", null: false
     t.string "text", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -25,7 +33,7 @@ ActiveRecord::Schema.define(version: 2019_02_16_084731) do
   create_table "users", force: :cascade do |t|
     t.string "twitter_identifier", null: false
     t.string "screen_name", null: false
-    t.string "name", null: false
+    t.string "name", default: "", null: false
     t.string "desription", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

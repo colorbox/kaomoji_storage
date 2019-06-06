e498b1782e9f08baf55ff1e80f3782a6662fc918
colorbox <colorbox222@gmail.com>
2019-06-06 17:43:23 +0900

    Add filtering kaomoji methods based on unicode

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -37,4 +37,113 @@ class Tweet < ApplicationRecord
     kaomojis << (duped.slice!( kaomoji_regexp )) while duped.slice( kaomoji_regexp )
     kaomojis
   end
+
+  def kaomoji_parts?(chr)
+    kaomoji_group_categories = %w(Pc Pd Pe Pf Pi Po Ps Sc Sk Sm So)
+    japanese_group = %w(Basic\ Latin Hiragana CJK\ Unified\ Ideographs Fullwidth\ ASCII\ Variants)
+    parts = %w(\( \) （ ） ・)
+    return !japanese_group.include?(Unicode::Blocks.blocks(chr).first) || kaomoji_group_categories.include?(Unicode::Categories.categories(chr).first)
+  end
+
+  def normal_char?(chr)
+    normal_char_blocks = %w(Lc Ll Lm Lo Lt Lu Nd Nl)
+    target_block = Unicode::Categories.categories(chr)
+    normal_char_blocks.include?(target_block.first)
+  end
+
+  def create_unicode_kaomojis
+    get_unicode_kaomojis.each do |kaomoji|
+      kaomojis.create(kaomoji: kaomoji) unless kaomojis.map(&:kaomoji).include?(kaomoji)
+    end
+  end
+
+  def get_unicode_kaomojis
+    return [] if text.chars.length.zero?
+
+    kaomojis = (0..text.chars.size-1).map{|i| get_one_unicode_kaomoji_at_index(i) if kaomoji_parts?(text.chars[i]) }.uniq.compact
+    kaomojis.select{|k|valid_kaomoji?(k)}
+  end
+
+  def get_one_unicode_kaomoji_at_index(index)
+    start = get_kaomoji_left_side(index)
+    last = get_kaomoji_right_side(index)
+    text.chars[start..last].join
+  end
+
+  THRESHOLD=2
+
+  def get_kaomoji_right_side(start_index)
+    return start_index if start_index==text.chars.length-1
+    last = start_index
+    count = 0
+    text.chars[start_index..text.length-1].each_with_index do |chr, index|
+      if kaomoji_parts?(chr)
+        count = 0
+        last = start_index + index
+      elsif normal_char?(chr)
+        count += 1
+      end
+      return last if count > THRESHOLD || index == text.chars[start_index..text.length-1].length - 1
+    end
+  end
+
+  def get_kaomoji_left_side(start_index)
+    return 0 if start_index.zero?
+    first = start_index
+    count = 0
+    text.chars[0..start_index].reverse.each_with_index do |chr, index|
+      if kaomoji_parts?(chr)
+        count = 0
+        first = start_index - index
+      elsif normal_char?(chr)
+        count += 1
+      end
+      return first if count > THRESHOLD || index == start_index-1
+    end
+  end
+
+  def unicode_kaomojis(threshold=2)
+    kaomojis = []
+    current=[]
+    count=0
+    text.split('').each do |chr|
+      if kaomoji_parts?(chr)
+        current.push(chr)
+        count=0
+      else
+        count+=1
+      end
+
+      pp [chr,count]
+      if count > threshold
+        kaomoji = current.join
+        kaomojis.push(kaomoji) if valid_kaomoji?(kaomoji)
+        current=[]
+        count=0
+      end
+    end
+
+    if current.count > 0
+      kaomoji = current.join
+      kaomojis.push(kaomoji) if valid_kaomoji?(kaomoji)
+    end
+
+    kaomojis
+  end
+
+  def valid_kaomoji?(kaomoji)
+    kaomoji.length > 3 && !half_of_normal_chars(kaomoji) && !part_of_tico_address?(kaomoji)
+  end
+
+  def half_of_normal_chars(str)
+    normal_chars = %w(Lc Ll Lm Lo Lt Lu Nd Nl)
+    normal_words_count =str.split('').map{|chr|Unicode::Categories.categories(chr).map{|block|normal_chars.include?(block)}.any? ? 1 : 0}.sum.to_f
+    all_count = str.length.to_f
+    result = (normal_words_count / all_count) > 0.5
+    result
+  end
+
+  def part_of_tico_address?(kaomoji)
+    kaomoji.include?('://t.c')
+  end
 end
--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -1,8 +1,9 @@
 require 'test_helper'
 
 class TweetTest < ActiveSupport::TestCase
-  test 'fetch_kaomoji' do
-
-    assert_equal tweets(:one).kaomojis, ['(-3-)', '(・3・)']
+  test 'get kaomoji index' do
+    assert_equal 8, tweets(:two).get_kaomoji_right_side(4)
+    assert_equal 4 , tweets(:two).get_kaomoji_left_side(8)
+    assert_equal ['(-3-)', '(・3・)'] , tweets(:two).get_unicode_kaomojis
   end
 end

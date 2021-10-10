b77ed9b986739eeab989610600fab15e729c4446
colorbox <colorbox222@gmail.com>
2021-10-10 22:46:33 +0900

    Add oneline kaomoji filter

--- app/controllers/kaomojis_controller.rb
+++ app/controllers/kaomojis_controller.rb
@@ -2,6 +2,14 @@
 
 class KaomojisController < ApplicationController
   def index
-    @kaomojis = UniqueKaomoji.order(:kaomoji).page params[:page]
+    @kaomojis = UniqueKaomoji.order(:kaomoji)
+    @kaomojis = @kaomojis.oneline if oneline
+    @kaomojis = @kaomojis.page params[:page]
+  end
+
+  private
+
+  def oneline
+    params[:oneline]
   end
 end
--- app/models/unique_kaomoji.rb
+++ app/models/unique_kaomoji.rb
@@ -1,4 +1,7 @@
 class UniqueKaomoji < ApplicationRecord
+
+  scope :oneline, -> { where("kaomoji not like '%\n%'") }
+
   def select!
     update!(selected: true)
   end
